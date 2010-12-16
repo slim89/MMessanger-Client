1f85783e909e3405932c0708107145859d7675de
Natalya <Natalya.Filinova@teleca.com>
2010-12-16 15:01:07 +0300

    1. Deleted MTheme::loadCSS and MTheme::addPixmapDirectory from main.cpp. It is not needed. The theme will be taken from /usr/share/themes/base//meegotouch/Meego-KVP-Client/style/

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
     MainWindow w;
     //MTheme::loadCSS("usr/share/Meego-KVP-Client/style/Meego-KVP-Client.css");
     //MTheme::addPixmapDirectory("usr/share/Meego-KVP-Client/style/");
-    QString client_style_dir(KVP_THEMES_STYLE_DIR);
-    QString client_css = client_style_dir + KVP_THEMES_CSS;
+    //QString client_style_dir(KVP_THEMES_STYLE_DIR);
+    //QString client_css = client_style_dir + KVP_THEMES_CSS;
 
-    MTheme::loadCSS( client_style_dir  );
-    MTheme::addPixmapDirectory( client_css );
+    //MTheme::loadCSS( client_style_dir  );
+    //MTheme::addPixmapDirectory( client_css );
 
     w.show();
     return a.exec();
